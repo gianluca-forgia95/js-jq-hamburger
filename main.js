57aba1e153632d827b11f8bd3a03678539598c7e
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-11 15:01:16 +0100

    al click della x lo nascondo

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ mostrare / nascondere il menu principale
 //Variabili utili
 var menuResp = $('.hamburger-menu');
 var burgerBtn = $('.header-right > a');
+var crossMenu = $('.close');
 
-burgerBtn.click(
-  function () {
-  menuResp.show();
-  }
-);
+ //Mostro il menu al click sul burger
+ burgerBtn.click(
+   function () {
+   menuResp.show();
+   }
+ );
+
+ //Lo nascondo al click sulla x
+ crossMenu.click(
+   function () {
+     menuResp.hide();
+   }
+ );
